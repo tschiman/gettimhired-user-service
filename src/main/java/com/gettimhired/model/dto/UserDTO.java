5e56a43a0212656da5d81991a188397f46f7979b
Tim Schimandle <tschiman@gmail.com>
2024-07-17 13:30:32 -0600

    Update the userDTO to have a class for data transfer

--- src/main/java/com/gettimhired/model/dto/UserDTO.java
+++ src/main/java/com/gettimhired/model/dto/UserDTO.java
@@ -4,7 +4,8 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record SignUpFormDTO(
+public record UserDTO(
+        String id,
         @NotBlank
         @Email
         String email,
